d4576e350487dac6de84ee65bab71bbef61353b7
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-03-31 01:28:02 +0300

    Add project files.

---
+++ AtYarisiUygulamasi/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace AtYarisiUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.atRuzgarGulu = new System.Windows.Forms.PictureBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.atYagiz = new System.Windows.Forms.PictureBox();
+            this.atPoyraz = new System.Windows.Forms.PictureBox();
+            this.atGolgeYele = new System.Windows.Forms.PictureBox();
+            this.atAlver = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgarGulu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atYagiz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atGolgeYele)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atAlver)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // atRuzgarGulu
+            // 
+            this.atRuzgarGulu.Image = ((System.Drawing.Image)(resources.GetObject("atRuzgarGulu.Image")));
+            this.atRuzgarGulu.Location = new System.Drawing.Point(12, 53);
+            this.atRuzgarGulu.Name = "atRuzgarGulu";
+            this.atRuzgarGulu.Size = new System.Drawing.Size(182, 79);
+            this.atRuzgarGulu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atRuzgarGulu.TabIndex = 0;
+            this.atRuzgarGulu.TabStop = false;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(12, 12);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(182, 23);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "BAŞLAT";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // atYagiz
+            // 
+            this.atYagiz.Image = ((System.Drawing.Image)(resources.GetObject("atYagiz.Image")));
+            this.atYagiz.Location = new System.Drawing.Point(12, 138);
+            this.atYagiz.Name = "atYagiz";
+            this.atYagiz.Size = new System.Drawing.Size(182, 79);
+            this.atYagiz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atYagiz.TabIndex = 0;
+            this.atYagiz.TabStop = false;
+            // 
+            // atPoyraz
+            // 
+            this.atPoyraz.Image = ((System.Drawing.Image)(resources.GetObject("atPoyraz.Image")));
+            this.atPoyraz.Location = new System.Drawing.Point(12, 223);
+            this.atPoyraz.Name = "atPoyraz";
+            this.atPoyraz.Size = new System.Drawing.Size(182, 79);
+            this.atPoyraz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atPoyraz.TabIndex = 0;
+            this.atPoyraz.TabStop = false;
+            // 
+            // atGolgeYele
+            // 
+            this.atGolgeYele.Image = ((System.Drawing.Image)(resources.GetObject("atGolgeYele.Image")));
+            this.atGolgeYele.Location = new System.Drawing.Point(12, 308);
+            this.atGolgeYele.Name = "atGolgeYele";
+            this.atGolgeYele.Size = new System.Drawing.Size(182, 79);
+            this.atGolgeYele.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atGolgeYele.TabIndex = 0;
+            this.atGolgeYele.TabStop = false;
+            // 
+            // atAlver
+            // 
+            this.atAlver.Image = ((System.Drawing.Image)(resources.GetObject("atAlver.Image")));
+            this.atAlver.Location = new System.Drawing.Point(12, 393);
+            this.atAlver.Name = "atAlver";
+            this.atAlver.Size = new System.Drawing.Size(182, 79);
+            this.atAlver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atAlver.TabIndex = 0;
+            this.atAlver.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.AliceBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(811, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 478);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FINISH";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label2.Location = new System.Drawing.Point(12, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(793, 10);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label3.Location = new System.Drawing.Point(12, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(793, 10);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label4.Location = new System.Drawing.Point(12, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(793, 10);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label5.Location = new System.Drawing.Point(12, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(793, 10);
+            this.label5.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label6.Location = new System.Drawing.Point(12, 475);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(793, 10);
+            this.label6.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(838, 499);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.atAlver);
+            this.Controls.Add(this.atGolgeYele);
+            this.Controls.Add(this.atPoyraz);
+            this.Controls.Add(this.atYagiz);
+            this.Controls.Add(this.atRuzgarGulu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgarGulu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atYagiz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atGolgeYele)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atAlver)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox atRuzgarGulu;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.PictureBox atYagiz;
+        private System.Windows.Forms.PictureBox atPoyraz;
+        private System.Windows.Forms.PictureBox atGolgeYele;
+        private System.Windows.Forms.PictureBox atAlver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ AtYarisiUygulamasi/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisiUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            AtDurumDegistir(true);
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtDurumDegistir(false);
+        }
+
+        void AtDurumDegistir(bool durum)
+        {
+            atAlver.Enabled = atGolgeYele.Enabled = atPoyraz.Enabled = atYagiz.Enabled = atRuzgarGulu.Enabled = durum;
+        }
+
+        void AtDurumDegistir(bool durum, PictureBox at)
+        {
+            atAlver.Enabled = atGolgeYele.Enabled = atPoyraz.Enabled = atYagiz.Enabled = atRuzgarGulu.Enabled = durum;
+            at.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            foreach (Control item in this.Controls)
+            {
+                if (item is PictureBox)
+                {
+                    item.Left += rnd.Next(1, 10);
+                }
+            }
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is PictureBox)
+                {
+                    Finish((PictureBox)item);
+                }
+            }
+        }
+
+        void Finish(PictureBox at)
+        {
+            if (at.Right - 18 >= label1.Left)
+            {
+                timer1.Stop();
+                AtDurumDegistir(false, at);
+                MessageBox.Show("Kazanan At :" + " " + at.Name);
+            }
+        }
+    }
+}
